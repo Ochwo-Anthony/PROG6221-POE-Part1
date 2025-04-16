23beff5edf27e6f1cbb5477a7a8ce223f8ff37f0
ST10395938 <130601150+Ochwo-Anthony@users.noreply.github.com>
2025-04-16 15:24:02 +0200

    Second Commit - Project Created
    Project created.

---
+++ ST10395938-PROG6221-POEPart1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ST10395938_PROG6221_POEPart1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
